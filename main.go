04bd886eff49e1e9608458cfe44df06974cbb7f9
ThePhiri <chipoephiri@gmail.com>
2022-08-07 16:58:32 +0200

    add root endpoint in main

--- main.go
+++ main.go
@@ -30,4 +30,11 @@ func main() {
 		log.Fatal("Error app failed to start")
 		panic(err)
 	}
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"success": true,
+			"message": "You are at the root endpoint 😉",
+		})
+	})
 }
