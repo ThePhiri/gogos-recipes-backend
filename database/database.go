c276f87fe6f47648d89201881b25d179e8d8e5f0
ThePhiri <chipoephiri@gmail.com>
2022-08-07 18:21:30 +0200

    change error from fatal to Print

--- database/database.go
+++ database/database.go
@@ -24,7 +24,8 @@ func Connect() {
 	if os.Getenv("APP_ENV") != "production" {
 		err := godotenv.Load()
 		if err != nil {
-			log.Fatal("Error loading .env file")
+			log.Print("Error loading .env file")
+
 		}
 
 	}
